0f3299b5270beea60af516d3b4eadb93c41ca9b1
TimmyGOGO <temich941@mail.ru>
2019-01-09 10:54:27 +0300

    Basics in 1 file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
+import { createStore } from 'redux';
+
+// функция для управления данными store:
+function playlist(state = [], action) {
+  if (action.type === 'ADD_TRACK') {
+    return [
+      ...state,
+      action.payload
+    ];
+  }
+  return state;
+}
+
+// создать store:
+const store = createStore(playlist);
+
+// получить состояние:
+console.log(store.getState());
+
+// подписаться на store: (получаем состояние при каждом изменении store)
+store.subscribe(() => {
+  console.log('subscribe', store.getState());
+});
+
+// функция, меняющая значение в store:
+store.dispatch({ type: 'ADD_TRACK', payload: 'Smells like spirit' });
+store.dispatch({ type: 'ADD_TRACK', payload: 'Enter Sandman' });
