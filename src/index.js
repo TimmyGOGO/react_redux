c6e7b45742be23786344e791e30ab8225c13cb06
TimmyGOGO <temich941@mail.ru>
2019-01-09 17:03:10 +0300

    Routing: connected-react-router + react-router.

--- src/index.js
+++ src/index.js
@@ -3,27 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux'; 
-import reducer from './reducers';
+
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import { Router, Route} from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
+
+import createRootReducer from './reducers';
+import { createBrowserHistory } from 'history';
+import { routerMiddleware } from 'connected-react-router'
+import { ConnectedRouter } from 'connected-react-router';
+import { Route, Switch } from 'react-router';
+
+const history = createBrowserHistory();
 
 // таким образом composeWithDevTools добавляет автоматически devtools для включения плагина 
 // redux devtools и при этом можно добавить еще middleware через запятую:
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(createRootReducer(history), 
+                          composeWithDevTools(applyMiddleware(thunk, routerMiddleware(history))));
 
-// const history = syncHistoryWithStore(Route.hashHistory, store);
+
 
 ReactDOM.render(
   <Provider store={store}>
-    {/* <Router history={Route.hashHistory}>
-      <Route path="/" component={App}/>
-      <Route path="/about" component={About}/>
-      <Route path="/tracks/:id" component={Track}/>
-    </Router> */}
-    <App />
+    <ConnectedRouter history={history}>
+      <>
+        <Switch>
+          <Route exact path="/" component={App} />
+        </Switch>
+      </>
+    </ConnectedRouter>
+    {/* <Route path="/about" component={About}/>
+      <Route path="/tracks/:id" component={Track}/> */}
+    {/*</Router> */}
   </Provider>, 
   document.getElementById('root')
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
+import { connectRouter } from 'connected-react-router';
 
 import tracks from './tracks';
 import playlists from './playlists';
 import filterTracks from './filterTracks';
 
-export default combineReducers({
-  routing: routerReducer,
+export default (history) => combineReducers({
+  router: connectRouter(history),
   tracks,
   playlists,
   filterTracks
